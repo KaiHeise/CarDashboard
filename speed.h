c46b2d138087b1d37252e34e19a370baf2984e6f
KaiHeise <116730981+KaiHeise@users.noreply.github.com>
2022-10-26 19:53:41 +0200

    Add files via upload

---
+++ speed.h
@@ -0,0 +1,12 @@
+#ifndef SPEED_H
+#define SPEED_H
+
+#include "auto.h"
+
+class Speed : public Auto
+{
+public:
+    Speed();
+};
+
+#endif // SPEED_H
---
+++ tempo.h
@@ -0,0 +1,17 @@
+#ifndef TEMPO_H
+#define TEMPO_H
+
+#include "auto.h"
+
+// sollte umgekehrt, nicht hier auto, sondern dort ... includieren?
+
+
+class Tempo : public Auto
+{
+public:
+    Tempo();
+};
+
+ // float getTempo();
+
+#endif // TEMPO_H
